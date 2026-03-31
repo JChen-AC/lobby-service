c27503664a5a837d91f1740b764372120da12686
JChen-AC <chen1101@algonquinlive.com>
2026-03-31 01:53:59 -0400

    Adding environment variables

--- lobby-service/src/app.js
+++ lobby-service/src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
+import 'dotenv/config';
 
 const app = express();
-const PORT = 4000;
+//const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
+
 
 app.use(cors());
 app.use(express.json());
